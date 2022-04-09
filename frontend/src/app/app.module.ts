670be09040ad5aa4b99a85722fa205bdfaed5c04
Richard <richard.tarassov@hotmail.com>
2022-04-09 13:18:14 +0300

    Revert "Revert "[change] - Reading posts from server works!""
    This reverts commit 406b4f82bf4c171697a059afd52da07d901e165f.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { NewsFeedComponent } from './components/news-feed/news-feed.component';
 
 @NgModule({
   declarations: [
-    WelcomeComponent
+    WelcomeComponent,
+    NewsFeedComponent
   ],
   imports: [
     BrowserModule,
